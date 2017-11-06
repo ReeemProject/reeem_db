8b6a9e0ec5bbbcef8aec133546d88f423f520b0e
Ludwig Huelk <Ludwig.Huelk@rl-institut.de>
2017-11-06 11:15:33 +0100

    add table for pathways

---
+++ database_setup/reeem_db_pathways.sql
@@ -0,0 +1,10 @@
+/*
+database views
+
+__copyright__   = "© Reiner Lemoine Institut"
+__license__     = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__         = "https://www.gnu.org/licenses/agpl-3.0.en.html"
+__author__      = "Ludwig Hülk"
+*/
+
+
