c8c3813e080b8781d64d32db1eb035baacad277e
Ludee <Ludwig.Huelk@rl-institut.de>
2018-06-14 11:31:58 +0200

    add metadata for ecosystem model data #35

---
+++ database_setup/reeem_db_setup_ecosystem.sql
@@ -0,0 +1,89 @@
+/*
+Ecosystem  Table Setup
+
+__copyright__   = "© Reiner Lemoine Institut"
+__license__     = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__         = "https://www.gnu.org/licenses/agpl-3.0.en.html"
+__author__      = "Ludwig Hülk"
+__issue__       = "https://github.com/ReeemProject/reeem_db/issues/35"
+
+ * This file is part of project REEEM (https://github.com/ReeemProject/reeem_db).
+ * It's copyrighted by the contributors recorded in the version control history:
+ * ReeemProject/reeem_db/blob/master/database_setup/utilities/reeem_example_table.sql
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+*/
+
+
+-- Ecosystem output
+DROP TABLE IF EXISTS    model_draft.reeem_ecosystem CASCADE;
+CREATE TABLE            model_draft.reeem_ecosystem (
+    "id"            serial NOT NULL,
+    "pathway"       text,
+    "version"       text,
+    "schema"        text,
+    "category"      text,
+    "tags"          hstore,
+    "region"        text,
+    "year"          smallint,
+    "indicator"     text,
+    "value"         double precision,
+    "unit"          text,
+    "aggregation"   boolean,
+    "updated"       timestamp with time zone,
+    "source"        text,
+    CONSTRAINT reeem_ecosystem_pkey PRIMARY KEY (id) );
+
+-- access rights
+ALTER TABLE             model_draft.reeem_ecosystem OWNER TO reeem_user;
+GRANT SELECT ON TABLE   model_draft.reeem_ecosystem TO reeem_read WITH GRANT OPTION;
+
+-- metadata
+COMMENT ON TABLE model_draft.reeem_ecosystem IS 
+    '{"title": "",
+    "description": "",
+    "language": [ "eng", "ger" ],
+    "spatial": 
+        {"location": "",
+        "extent": "",
+        "resolution": ""},
+    "temporal": 
+        {"reference_date": "",
+        "start": "",
+        "end": "",
+        "resolution": ""},
+    "sources": [
+        {"name": "", "description": "", "url": "", "license": "", "copyright": ""},
+        {"name": "", "description": "", "url": "", "license": "", "copyright": ""} ],
+    "license":
+        {"id": "",
+        "name": "",
+        "version": "",
+        "url": "",
+        "instruction": "",
+        "copyright": ""},
+    "contributors": [
+        {"name": "Ludee", "email": "none", "date": "2017--", "comment": "Create table"},
+        {"name": "", "email": "", "date": "", "comment": ""} ],
+    "resources": [
+        {"name": "model_draft.reeem_model_data_template",        
+        "format": "PostgreSQL",
+        "fields": [
+            {"name": "id", "description": "Unique identifier", "unit": "none"},
+            {"name": "pathway", "description": "REEEM pathway", "unit": "none"},
+            {"name": "version", "description": "REEEM version", "unit": "none"},
+            {"name": "schema", "description": "1. classification", "unit": "none"},
+            {"name": "category", "description": "2. classification", "unit": "none"},
+            {"name": "tags", "description": "Free classification", "unit": "none"},
+            {"name": "region", "description": "Country or region", "unit": "none"},
+            {"name": "year", "description": "Year", "unit": "none"},
+            {"name": "indicator", "description": "Parameter name", "unit": "none"},
+            {"name": "value", "description": "Parameter value", "unit": "unit"},
+            {"name": "unit", "description": "Parameter unit", "unit": "none"},
+            {"name": "aggregation", "description": "True if aggregated", "unit": "none"},
+            {"name": "updated", "description": "Timestamp", "unit": "none"},
+            {"name": "source", "description": "Data source", "unit": "none"} ] } ],
+    "metadata_version": "1.3"}';
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('REEEM','v0.1.0','setup','model_draft','reeem_ecosystem','reeem_db_setup_ecosystem.sql',' ');
