e1eb4eec50a440b729fcfb09f1727c80a160e0e7
Ludee <Ludwig.Huelk@rl-institut.de>
2018-10-08 10:47:45 +0200

    update metadata sources #4

--- database_setup/reeem_db_setup_times_paneu.sql
+++ database_setup/reeem_db_setup_times_paneu.sql
@@ -77,7 +77,7 @@ COMMENT ON TABLE model_draft.reeem_times_paneu_input IS
         {"name": "Ludee", "email": "none", "date": "2018-01-30", "comment": "New naming structure"},
         {"name": "Ludee", "email": "none", "date": "2018-02-27", "comment": "Simplify structure"} ],
     "resources": [
-        {"name": "model_draft.reeem_times_paneu_input",        
+        {"name": "model_draft.reeem_times_paneu_input",
         "format": "PostgreSQL",
         "fields": [
             {"name": "id", "description": "Unique identifier", "unit": "none"},
@@ -140,7 +140,7 @@ COMMENT ON TABLE model_draft.reeem_times_paneu_output IS
         "end": "2050",
         "resolution": "5 years"},
     "sources": [
-        {"name": "", "description": "", "url": "", "license": "", "copyright": ""} ],
+        {"name": "TIMES PanEU", "description": "TIMES is an energy system model generator which is a further development of the model generators MARKAL and EFOM-ENV, written in GAMS. TIMES is a bottom-up linear optimization model based on a technical approach. The aim of the model is to optimize the energy system cost according to the given energy demands, energy technologies, and policy requirements. The Pan-European TIMES energy system model (TIMES PanEU) is a 30 region partial equilibrium energy system model. The model covers the EU-28 countries, with addition of Norway and Switzerland. The base year is 2010 and the time horizon ends in 2050. The current structure accounts for 12 time slices, 4 seasonal and 3 daily.  The model is split in 5-year time steps. Both greenhouse gas emissions (CO2, CH4, N2O, SF6) and local air pollutant emissions (SO2, CO, NOx, NMVOC, PM10, PM2.5) are included. TIMES PanEU contains the country specific data covering all the sectors related to energy supply and demand. The system commences from the potential of different energy sources in a particular country and includes public and industrial generation of electricity, industry, agriculture, refineries, inventory power stations and the end-use service demands such as heating, lighting and transportation.", "url": "http://openenergyplatform.org/factsheets/models/41/", "license": "none", "copyright": "none"} ],
     "license":
         {"id": "ODC-BY-1.0",
         "name": "Open Data Commons Attribution License 1.0",
@@ -156,9 +156,10 @@ COMMENT ON TABLE model_draft.reeem_times_paneu_output IS
         {"name": "Ludee", "email": "none", "date": "2017-11-06", "comment": "Include pathway"},
         {"name": "Ludee", "email": "none", "date": "2018-01-30", "comment": "New naming structure"},
         {"name": "Ludee", "email": "none", "date": "2018-02-27", "comment": "Simplify structure"},
-        {"name": "Ludee", "email": "none", "date": "2018-03-15", "comment": "Add license"}],
+        {"name": "Ludee", "email": "none", "date": "2018-03-15", "comment": "Add license"},
+        {"name": "Ludee", "email": "none", "date": "2018-10-08", "comment": "Add source"}],
     "resources": [
-        {"name": "model_draft.reeem_times_paneu_output",        
+        {"name": "model_draft.reeem_times_paneu_output",
         "format": "PostgreSQL",
         "fields": [
             {"name": "id", "description": "Unique identifier", "unit": "none"},
