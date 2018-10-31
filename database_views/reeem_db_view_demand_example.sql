88cebe985e961f17d93d9fcd752f9857687928d5
Ludee <Ludwig.Huelk@rl-institut.de>
2018-10-31 16:17:23 +0100

    add example view #37

---
+++ database_views/reeem_db_view_demand_example.sql
@@ -0,0 +1,41 @@
+/*
+REEEM-DB - View - Energy Demand - Example
+
+__copyright__   = "© Reiner Lemoine Institut"
+__license__     = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__         = "https://www.gnu.org/licenses/agpl-3.0.en.html"
+__author__      = "Ludwig Hülk"
+
+ * This file is part of project REEEM (https://github.com/ReeemProject/reeem_db).
+ * It's copyrighted by the contributors recorded in the version control history:
+ * ReeemProject/reeem_db/database_setup/reeem_db_setup_energypro.sql
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+*/
+
+
+-- view
+CREATE OR REPLACE VIEW model_draft.reeem_db_energydemand_view AS
+    SELECT  id,
+            pathway,
+            framework,
+            version,
+            region,
+            year,
+            value,
+            unit,
+            aggregation,
+            tags,
+            updated
+    FROM    model_draft.reeem_times_paneu_input
+    WHERE   pathway = 'Base' AND
+            region = 'SE' AND
+            tags @> '"model"=>"times_paneu"'::hstore AND
+            tags @> '"field"=>"service_demand"'::hstore AND
+            tags @> '"demand_sector"=>"agriculture"'::hstore
+    ORDER BY pathway, version, region, year;
+
+ALTER TABLE model_draft.reeem_db_energydemand_view OWNER TO reeem_user; 
+
+-- scenario log (version,io,schema_name,table_name,script_name,comment)
+SELECT reeem_scenario_log('v0.3.0','view','model_draft','reeem_db_energydemand_view','reeem_db_view_demand_example.sql',' ');
