e746393ed8676d68021a625fe0494b8ed88a2d25
Ludee <Ludwig.Huelk@rl-institut.de>
2018-11-27 12:14:28 +0100

    data dif for test view #37

--- database_views/reeem_db_view_demand_example.sql
+++ database_views/reeem_db_view_demand_example.sql
@@ -23,6 +23,8 @@ CREATE VIEW         model_draft.reeem_db_energydemand_view AS
             version,
             region,
             year,
+            category,
+            indicator,
             value,
             unit,
             aggregation,
@@ -43,3 +45,24 @@ ALTER TABLE model_draft.reeem_db_energydemand_view OWNER TO reeem_user;
 
 -- scenario log (version,io,schema_name,table_name,script_name,comment)
 SELECT reeem_scenario_log('v0.3.0','view','model_draft','reeem_db_energydemand_view','reeem_db_view_demand_example.sql',' ');
+
+
+-- Difference calculation
+    WITH v1 AS (SELECT "version","region","year","category","indicator","value","unit","updated"
+        FROM    model_draft.reeem_db_energydemand_view
+        WHERE   pathway = 'Base'), --AND year = '2010' AND "category" = 'Service demands agriculture'
+        v2 AS (SELECT "version","region","year","category","indicator","value","unit","updated"
+        FROM    model_draft.reeem_db_energydemand_view
+        WHERE   pathway = 'HighRES')
+    SELECT  v1."category",v1."year",v1."indicator",
+            v1."region",-- AS region_1,v2."region" AS region_2,
+            v1."value" AS value_1,v2."value"AS value_2,
+            v1."unit",v1."updated"
+    FROM    v1 INNER JOIN v2 ON (v1.region = v2.region AND v1.category = v2.category AND v1.indicator = v2.indicator)
+    WHERE   v1.indicator = 'Agriculture' AND
+            --v1.year = '2010' AND
+            --v1.region = 'DE' AND
+            v1."value" <> v2."value"
+    ORDER BY region,year,"category";
+    
+    
