cc7727c5d9bcdcb7b2bdd18166575f85eda40bfb
Ludee <Ludwig.Huelk@rl-institut.de>
2019-01-09 15:57:41 +0100

    Update adapter for new model runs

--- database_adapter/reeem_adapter_osembe.py
+++ database_adapter/reeem_adapter_osembe.py
@@ -10,15 +10,76 @@ __version__ = "v0.1.3"
 from reeem_io import *
 
 # input
-# filename = "2017-02-07_Test_OSEMBE_FrameworkNA_DataV1_Output.xlsx"
-# filename = "2018-09-10_Base_OSEMBE_FrameworkNA_DataV1_Output.xlsx"
-filename = "2018-11-27_Base_OSeMBE_FrameworkNA_DataV1_Output.xlsx"
+filenames = [
+#           '2017-02-07_Test_OSEMBE_FrameworkNA_DataV1_Output.xlsx',
+#           '2018-09-10_Base_OSEMBE_FrameworkNA_DataV1_Output.xlsx',
+#           '2018-11-27_Base_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+
+            '2019-01-03_C0T0E0_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E1_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E2_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E3_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E4_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E5_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E6_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E7_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E8_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E9_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E10_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E11_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E12_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E13_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E14_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E15_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E16_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E17_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E18_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E19_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E20_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E21_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E22_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E23_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E24_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E25_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T0E26_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E0_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E1_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E2_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E3_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E4_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E5_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E6_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E7_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E8_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E9_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E10_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E11_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E12_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E13_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E14_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E15_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-07_C0T1E16_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E17_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E18_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E19_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E20_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E21_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E22_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E23_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E24_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E25_OSeMBE_FrameworkNA_DataV1_Output.xlsx',
+            '2019-01-03_C0T1E26_OSeMBE_FrameworkNA_DataV1_Output.xlsx']
+
 
 # regions = ['EU28']
 # regions = ['EU28', 'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES',
 #            'FI', 'FR', 'GR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV',
 #            'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'UK']
-regions = ['EU30', 'AT', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 
+# regions = ['EU30', 'AT', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 
+#            'ES', 'FI', 'FR', 'GR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 
+#            'LV', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 
+#            'UK']
+regions = ['EU+CH+NO', 'AT', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 
            'ES', 'FI', 'FR', 'GR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 
            'LV', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 
            'UK']
@@ -96,41 +157,47 @@ def osembe_2_reeem_db(filename, fns, empty_rows, db_schema, region, con):
 
 
 if __name__ == '__main__':
-    # file and table
-    fns = reeem_filenamesplit(filename)
-    
-    # i/o
-    if fns['io'] == "Input":
-        db_table = db_table_input
-    else:
-        db_table = db_table_output
     
     # logging
     log = logger()
     start_time = time.time()
     log.info('script started...')
-    log.info('...file: {}'.format(filename))
-    fns = reeem_filenamesplit(filename)
-    log.info('...pathway: {}'.format(fns['pathway']))
-    log.info('...model: {}'.format(fns['model']))
-    log.info('...framework: {}'.format(fns['framework']))
-    log.info('...version: {}'.format(fns['version']))
-    log.info('...i/o: {}'.format(fns['io']))
-    log.info('...regions: {}'.format(regions))
-    log.info('...establish database connection...')
-
+    
     # connection
     con = reeem_session()
     log.info('...read file(s)...')
+    
+    # import files
+    for filename in filenames:
+    
+        # file and table
+        fns = reeem_filenamesplit(filename)
+        
+        # i/o
+        if fns['io'] == "Input":
+            db_table = db_table_input
+        else:
+            db_table = db_table_output
+    
+        # log files
+        log.info('read file: {}'.format(filename))
+        log.info('...model: {}'.format(fns['model']))
+        log.info('...pathway: {}'.format(fns['pathway']))
+        log.info('...framework: {}'.format(fns['framework']))
+        log.info('...version: {}'.format(fns['version']))
+        log.info('...i/o: {}'.format(fns['io']))
+        log.info('...regions: {}'.format(regions))
+        log.info('...database table: model_draft.{}'.format(db_table))
 
-    # import
-    for region in regions:
-        osembe_2_reeem_db(filename, fns, empty_rows,
-                         db_schema, region, con)
+        # import
+        for region in regions:
+            osembe_2_reeem_db(filename, fns, empty_rows,
+                            db_schema, region, con)
 
-    # scenario log
-    scenario_log(con, 'REEEM', __version__, 'import', db_schema, db_table,
-                 os.path.basename(__file__), filename)
+        # scenario log
+        scenario_log(con, 'REEEM', __version__, 'import', db_schema, db_table,
+                    os.path.basename(__file__), filename)
+
 
     # close connection
     con.close()
