7645e89671bbc0df249b6cd3fffd5dc5d4ff02ca
Ludee <Ludwig.Huelk@rl-institut.de>
2019-01-11 16:19:40 +0100

    Include functions to remove pathways

--- database_setup/data-cleaning/reeem_db_remove_pathway.sql
+++ database_setup/data-cleaning/reeem_db_remove_pathway.sql
@@ -15,9 +15,27 @@ __issue__       = "https://github.com/ReeemProject/reeem_db/issues/4"
 */
 
 
--- delete pathway from OSeMBE
+-- delete pathway from OSeMBE 2019-01-09 
 DELETE FROM model_draft.reeem_osembe_output
     WHERE	pathway = 'C0T1E10';
 
 -- scenario log (project,version,io,schema_name,table_name,script_name,comment)
-SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_times_paneu_output','reeem_db_clean_times_paneu.sql',' ');
+SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_osembe_output','reeem_db_remove_pathway.sql',' ');
+
+
+
+-- delete pathway from TIMESPanEU 2019-01-11 
+DELETE FROM model_draft.reeem_times_paneu_input
+    WHERE	pathway = 'Base' AND framework = 'FrameworkV1' AND version = 'DataV4';
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_times_paneu_input','reeem_db_remove_pathway.sql',' ');
+
+
+DELETE FROM model_draft.reeem_times_paneu_output
+    WHERE	(pathway = 'Base' AND framework = 'FrameworkV1' AND version = 'DataV4') OR
+            (pathway = 'Base' AND framework = 'FrameworkV2' AND version = 'DataV2') OR
+            (pathway = 'HighRES' AND framework = 'FrameworkV1' AND version = 'DataV4');
+
+-- scenario log (project,version,io,schema_name,table_name,script_name,comment)
+SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_times_paneu_output','reeem_db_remove_pathway.sql',' ');
