59d6fb7dbd49c212e46d53fee5314375751a5f21
Ludee <Ludwig.Huelk@rl-institut.de>
2019-01-22 18:39:20 +0100

    check metadata #7

--- database_setup/reeem_db_setup_ecosense.sql
+++ database_setup/reeem_db_setup_ecosense.sql
@@ -23,20 +23,18 @@ DROP TABLE IF EXISTS    model_draft.reeem_ecosense_input CASCADE;
 CREATE TABLE            model_draft.reeem_ecosense_input (
     "id"            serial NOT NULL,
     "nid"           integer,
-    "dfid"          integer,
     "pathway"       text,
     "framework"     text,
     "version"       text,
     "region"        text,
-    "region_2"      text,
     "year"          smallint,
-    "category"      text,
     "indicator"     text,
     "value"         double precision,
     "unit"          text,
     "aggregation"   boolean,
     "tags"          hstore,
     "updated"       timestamp with time zone,
+    "source"        text,
     CONSTRAINT reeem_ecosense_input_pkey PRIMARY KEY (id) );
 
 -- access rights
@@ -77,20 +75,18 @@ COMMENT ON TABLE model_draft.reeem_ecosense_input IS
         "fields": [
             {"name": "id", "description": "Unique identifier", "unit": "none"},
             {"name": "nid", "description": "Model id", "unit": "none"},
-            {"name": "dfid", "description": "Row id", "unit": "none"},
             {"name": "pathway", "description": "REEEM pathway", "unit": "none"},
             {"name": "framework", "description": "REEEM framework", "unit": "none"},
             {"name": "version", "description": "REEEM version", "unit": "none"},
             {"name": "region", "description": "Country (responsible for impacts)", "unit": "none"},
-            {"name": "region_2", "description": "Country (where impacts occur)", "unit": "none"},
             {"name": "year", "description": "Year", "unit": "none"},
-            {"name": "category", "description": "2. classification", "unit": "none"},
             {"name": "indicator", "description": "Parameter name", "unit": "none"},
             {"name": "value", "description": "Parameter value", "unit": "unit"},
             {"name": "unit", "description": "Parameter unit", "unit": "none"},
             {"name": "aggregation", "description": "True if aggregated", "unit": "none"},
             {"name": "tags", "description": "Free classification", "unit": "none"},
-            {"name": "updated", "description": "Timestamp", "unit": "none"} ] } ],
+            {"name": "updated", "description": "Timestamp", "unit": "none"},
+            {"name": "source", "description": "Data source", "unit": "none"} ] } ],
     "metadata_version": "1.3"}';
 
 -- scenario log (version,io,schema_name,table_name,script_name,comment)
