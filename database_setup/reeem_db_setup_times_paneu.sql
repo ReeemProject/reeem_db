421611f561d1e133da8965e72b03de15a47d7896
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-02-07 11:54:01 +0100

    Add missing columns to CREATE TABLE statement #4

--- database_setup/reeem_db_setup_times_paneu.sql
+++ database_setup/reeem_db_setup_times_paneu.sql
@@ -37,6 +37,8 @@ CREATE TABLE            model_draft.reeem_times_paneu_input (
     "tags"          hstore,
     "updated"       timestamp with time zone,
     "source"        text,
+    "schema"        text,
+    "field"         text,
     CONSTRAINT reeem_times_paneu_input_pkey PRIMARY KEY (id) );
 
 -- access rights
@@ -98,11 +100,6 @@ COMMENT ON TABLE model_draft.reeem_times_paneu_input IS
             {"name": "source", "description": "Data source", "unit": "none"} ] } ],
     "metadata_version": "1.3"}';
 
--- Add missing columns
-ALTER TABLE model_draft.reeem_times_paneu_input
-  ADD COLUMN "schema" text,
-  ADD COLUMN "field"  text;
-
 -- scenario log (project,version,io,schema_name,table_name,script_name,comment)
 SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_times_paneu_input','reeem_db_setup_times_paneu.sql',' ');
 
@@ -125,6 +122,8 @@ CREATE TABLE            model_draft.reeem_times_paneu_output (
     "aggregation"   boolean,
     "tags"          hstore,
     "updated"       timestamp with time zone,
+    "schema"        text,
+    "field"         text,
     CONSTRAINT reeem_times_paneu_output_pkey PRIMARY KEY (id) );
 
 -- access rights
@@ -185,11 +184,6 @@ COMMENT ON TABLE model_draft.reeem_times_paneu_output IS
             {"name": "tags", "description": "Free classification", "unit": "none"},
             {"name": "updated", "description": "Timestamp", "unit": "none"} ] } ],
     "metadata_version": "1.3"}';
-
--- Add missing columns
-ALTER TABLE model_draft.reeem_times_paneu_output
-  ADD COLUMN "schema" text,
-  ADD COLUMN "field"  text;
 
 -- scenario log (project,version,io,schema_name,table_name,script_name,comment)
 SELECT scenario_log('REEEM','v0.2.0','setup','model_draft','reeem_times_paneu_output','reeem_db_setup_times_paneu.sql',' ');
