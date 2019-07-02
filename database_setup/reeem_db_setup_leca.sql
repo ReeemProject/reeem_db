2069193240d1bc6542f03b8080ed0e752eeb3741
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-07-02 17:36:30 +0200

    Remove double entry tags in fields in metadata #9

--- database_setup/reeem_db_setup_leca.sql
+++ database_setup/reeem_db_setup_leca.sql
@@ -83,7 +83,6 @@ COMMENT ON TABLE model_draft.reeem_leca_input IS
             {"name": "schema", "description": "1. classification", "unit": "none"},
             {"name": "field", "description": "2. classification", "unit": "none"},
             {"name": "category", "description": "3. classification", "unit": "none"},
-            {"name": "tags", "description": "Free classification", "unit": "none"},
             {"name": "region", "description": "Country or region", "unit": "none"},
             {"name": "year", "description": "Year", "unit": "none"},
             {"name": "indicator", "description": "Parameter name", "unit": "none"},
